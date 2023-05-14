8fb7a1d9d8b01911bccba79813d8bc6b4445029e
Trần Ngọc Hiếu <hieutnhe15199@fpt.edu.vn>
2023-05-14 23:25:03 +0700

    update migration db

---
+++ BusinessObjects/MyDBContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyStoreDB"));
+        }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get;set; }
+        protected override void OnModelCreating(ModelBuilder optionBuilder)
+        {
+            optionBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Beverages"},
+                new Category { CategoryId = 2, CategoryName = "Condiments" },
+                new Category { CategoryId = 3, CategoryName = "Confections" },
+                new Category { CategoryId = 4, CategoryName = "Dairy Products" },
+                new Category { CategoryId = 5, CategoryName = "Grains/Cerearls" },
+                new Category { CategoryId = 6, CategoryName = "Meat/Poultry" },
+                new Category { CategoryId = 7, CategoryName = "Produce" },
+                new Category { CategoryId = 8, CategoryName = "Seefood" }
+            );
+        }
+
+    }
+}
---
+++ BusinessObjects/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class Product
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string ProductName { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public int UnitsInStoke { get; set; }
+        [Required]
+        public decimal UnitPrice { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
