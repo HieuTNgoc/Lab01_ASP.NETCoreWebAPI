be0ac724bd8e068009d709e432d48c5ceb8d8072
Trần Ngọc Hiếu <hieutnhe15199@fpt.edu.vn>
2023-05-15 01:04:10 +0700

    update repository

---
+++ DataAccess/ProductDAO.cs
@@ -0,0 +1,92 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ProductDAO
+    {
+        public static List<Product> GetProducts()
+        {
+            var listProducts = new List<Product>();
+            try
+            {
+                using (var context = new MyDBContext())
+                {
+                    listProducts = context.Products.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return listProducts;
+        }
+        public static Product FindProductById(int productId)
+        {
+            Product p = new Product();
+            try
+            {
+                using(var context = new MyDBContext())
+                {
+                    p = context.Products.SingleOrDefault(x => x.ProductId == productId);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return p;
+        }
+        public static void SaveProduct(Product p)
+        {
+            try
+            {
+                using (var context = new MyDBContext())
+                {
+                    context.Products.Add(p);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public static void UpdateProduct(Product p)
+        {
+            try
+            {
+                using (var context = new MyDBContext())
+                {
+                    context.Entry<Product>(p).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+                throw;
+            }
+        }
+        public static void DeleteProduct(Product p)
+        {
+            try
+            {
+                using (var context = new MyDBContext())
+                {
+                    var p1 = context.Products.SingleOrDefault(c=> c.ProductId == p.ProductId);
+                    context.Products.Remove(p1);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using BusinessObjects;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void DeleteProduct(Product p) => ProductDAO.DeleteProduct(p);
+
+        public List<Category> GetCategories() => CategoryDAO.GetCategories();
+
+        public Product GetProductById(int id) => ProductDAO.FindProductById(id);
+
+        public List<Product> GetProducts() => ProductDAO.GetProducts();
+
+        public void SaveProduct(Product p) => ProductDAO.SaveProduct(p);
+
+        public void UpdateProduct(Product p) => ProductDAO.UpdateProduct(p);
+    }
+}
