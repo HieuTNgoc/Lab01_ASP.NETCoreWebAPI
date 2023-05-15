fb50926bb343760425327b074f817255f91aa599
Trần Ngọc Hiếu <hieutnhe15199@fpt.edu.vn>
2023-05-15 23:18:15 +0700

    update acctivity 5

---
+++ ProjectManagementAPI/Controllers/ProductsControllers.cs
@@ -0,0 +1,51 @@
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Repositories;
+
+namespace ProjectManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsControllers : ControllerBase
+    {
+     
+        private IProductRepository repository = new ProductRepository();
+        // GET: api/Products
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts() => repository.GetProducts();
+
+        // POST: ProductsController/Products
+        [HttpPost]
+        public IActionResult PostProduct(Product p)
+        {
+            repository.SaveProduct(p);
+            return NoContent();
+        }
+
+        // GET: ProductsController/Delete/5
+        [HttpDelete("id")]
+        public IActionResult DeletaProduct(int id)
+        {
+            var p = repository.GetProductById(id);
+            if (p == null)
+            {
+                return NoContent();
+            }
+            repository.DeleteProduct(p);
+            return NoContent();
+        }
+
+        [HttpPut("id")]
+        public IActionResult UpdateProduct(int id, Product p)
+        {
+            var pTmp = repository.GetProductById(id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            repository.UpdateProduct(p);
+            return NoContent();
+        }
+
+    }
+}
